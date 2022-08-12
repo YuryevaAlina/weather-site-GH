2b4af1743a90b67c536424d533991bfe2353bf9c
Alina Yuryeva <yuryeva.alina@gmail.com>
2022-08-12 19:26:28 +0200

    Delete comment in index js 2

--- src/index2.js
+++ src/index2.js
@@ -118,29 +118,6 @@ fahrenheitLink.addEventListener("click", showFahrengate);
 
 let showTemperature = document.querySelector(".temperature"); */
 
-/*let weather = {
-  paris: {
-    temp: 19.7,
-    humidity: 80,
-  },
-  tokyo: {
-    temp: 17.3,
-    humidity: 50,
-  },
-  lisbon: {
-    temp: 30.2,
-    humidity: 20,
-  },
-  "san francisco": {
-    temp: 20.9,
-    humidity: 100,
-  },
-  oslo: {
-    temp: -5,
-    humidity: 20,
-  },
-};
-
 // write your code here
 
 /*let city = prompt("Enter a city");
